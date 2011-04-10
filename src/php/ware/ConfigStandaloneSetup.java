e151b9e394c57805f3b3caccfcb06abc8575c7a4
David Moises Paz Reyes <davidmpaz@gmail.com>
2011-04-10 11:28:12 -0400

    Initial commit
    Configuration DSL to extend and configure the
    cross cutting aspects of other DSLs.

---
+++ src/php/ware/ConfigStandaloneSetup.java
@@ -0,0 +1,14 @@
+
+package php.ware;
+
+/**
+ * Initialization support for running Xtext languages 
+ * without equinox extension registry
+ */
+public class ConfigStandaloneSetup extends ConfigStandaloneSetupGenerated{
+
+	public static void doSetup() {
+		new ConfigStandaloneSetup().createInjectorAndDoEMFRegistration();
+	}
+}
+
